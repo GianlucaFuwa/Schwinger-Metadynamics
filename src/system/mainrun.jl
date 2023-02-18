92ef517853391ba859e532d09d476f26876a0058
GianlucaFuwa <51165364+GianlucaFuwa@users.noreply.github.com>
2023-02-18 01:21:12 +0100

    Update mainrun.jl

--- src/system/mainrun.jl
+++ src/system/mainrun.jl
@@ -6,7 +6,7 @@ module Mainrun
 
     import ..System_parameters: Params,Params_set,parameterloading
     import ..Verbose_print: Verbose_,println_verbose,print2file
-    import ..Gaugefields: Gaugefield,recalc_Sg!,recalc_CV!
+    import ..Gaugefields: Gaugefield,recalc_Sg!,recalc_CV!,swap!
     import ..Metadynamics: Bias_potential
     import ..Measurements: Measurement_set,measurements,calc_weights
     import ..MC: tempering_swap!,sweep!,sweep_meta!
@@ -246,5 +246,27 @@ module Mainrun
         flush(verbose)
         return nothing
     end
+                
+    function tempering_swap!(field_main::Gaugefield,field_meta::Gaugefield,bias::Bias_potential,rng::Xoshiro,static::Bool)
+		main_ind = index(bias,field_main.CV)
+		meta_ind = index(bias,field_meta.CV)
+
+		ΔCV = field_meta.CV - field_main.CV
+		ΔSg = field_meta.Sg - field_main.Sg
+		ΔV = bias[meta_ind] - bias[main_ind]
+		ΔV_pen = penalty_potential(bias,field_meta.CV) - penalty_potential(bias,field_main.CV)
+		accept_swap = rand(rng) ≤ exp(ΔV+ΔV_pen)
+		if accept_swap
+			swap!(field_main,field_meta)
+			field_meta.CV -= ΔCV
+			field_main.CV += ΔCV
+			field_meta.Sg -= ΔSg
+			field_main.Sg += ΔSg
+			if ~static
+				update_bias!(bias,field_meta.CV)
+			end
+		end
+		return accept_swap
+	end 
 
 end
